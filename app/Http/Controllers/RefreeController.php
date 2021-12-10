e3788cc269e60a60a2b09e0b723e19043f2d0a1c
gech11 <getachewfikadu77@gmail.com>
2021-12-10 22:05:55 +0300

    initial commit

---
+++ app/Http/Controllers/RefreeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Refree;
+use App\Http\Requests\StoreRefreeRequest;
+use App\Http\Requests\UpdateRefreeRequest;
+
+class RefreeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreRefreeRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRefreeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Refree  $refree
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Refree $refree)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Refree  $refree
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Refree $refree)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateRefreeRequest  $request
+     * @param  \App\Models\Refree  $refree
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateRefreeRequest $request, Refree $refree)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Refree  $refree
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Refree $refree)
+    {
+        //
+    }
+}
